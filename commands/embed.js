3960f9f1427fddf2ac8708ee7c4b1d275e4ffdcb
Assassin-1234 <chordsandfrets@gmail.com>
2020-07-28 03:12:25 +0530

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setTitle('Invite Me! (click on this)')
         .setURL('https://discord.com/oauth2/authorize?client_id=722782077111631935&scope=bot&permissions=2146958847')
-        .setAuthor('Assassin#1234')
+        .setAuthor('**ModBot**')
         .setDescription('This is my first public bot ! Im adding 1 new feature ever day !')
         .setColor('#FF2D00')
         .setThumbnail('https://images.unsplash.com/photo-1586864387967-d02ef85d93e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
