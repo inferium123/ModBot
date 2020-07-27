566e7c506061b1a313d68448a9f98762196cb49a
Assassin-1234 <chordsandfrets@gmail.com>
2020-07-28 04:23:24 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const { watch } = require("fs");
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity(`${client.guilds.cache.size} servers!`);
+    client.user.setActivity(`${client.guilds.cache.size} servers!`)
 }
