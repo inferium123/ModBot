311cd7df0e948868d0ea99e0e1b401ecd0e69547
Assassin-1234 <chordsandfrets@gmail.com>
2020-07-28 03:04:07 +0530

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -2,19 +2,17 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     const embed = new Discord.MessageEmbed()
-        .setTitle('This is a test Embed.')
-        .setURL('http://youtube.com/dashcraft123')
-        .setAuthor('DashCraft')
-        .setDescription('This is a test Description which we will see.')
+        .setTitle('Invite Me! (click on this)')
+        .setURL('https://discord.com/oauth2/authorize?client_id=722782077111631935&scope=bot&permissions=2146958847')
+        .setAuthor('Assassin#1234')
+        .setDescription('This is my first public bot ! Im adding 1 new feature ever day !')
         .setColor('#FF2D00')
-        .setThumbnail('https://cdn.osxdaily.com/wp-content/uploads/2016/10/YouTube-icon-full_color-610x430.png')
-        .setImage('https://cdn.osxdaily.com/wp-content/uploads/2016/10/YouTube-icon-full_color-610x430.png')
-        .setFooter('This is a example footer', 'https://cdn.osxdaily.com/wp-content/uploads/2016/10/YouTube-icon-full_color-610x430.png')
+        .setThumbnail('https://images.unsplash.com/photo-1586864387967-d02ef85d93e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
+        .setImage('https://images.unsplash.com/photo-1586864387967-d02ef85d93e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
+        .setFooter('This is a example footer', 'https://images.unsplash.com/photo-1586864387967-d02ef85d93e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')
         .addFields(
             { name: 'Test Number 1', value: 'This is a example value', inline: true},
-            { name: 'Test Number 2', value: 'This is a example value', inline: true},
-            { name: 'Test Number 3', value: 'This is a example value'},
-            { name: 'Test Number 4', value: 'This is a example value'}
+            { name: 'Test Number 2', value: 'This is a example value', inline: true}
         )
         .setTimestamp()
 
@@ -23,9 +21,9 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.config = {
-    name: "embed",
+    name: "invite",
     description: "example of an Embed.",
-    usage: "?embed",
+    usage: "?invite",
     accessableby: "Members",
     aliases: []
 }
