cb5d9944eaf5831c81ef4838edeb6233a5ee37fa
Assassin-1234 <chordsandfrets@gmail.com>
2020-07-28 04:28:52 +0530

    status

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,9 @@
 const Discord = require("discord.js");
 const { watch } = require("fs");
+const bot = new Discord.Client();
+        bot.on()
+        const client = new Discord.Client();
+        client.on()
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
