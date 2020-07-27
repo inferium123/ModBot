23c7c372f78875112af17c783432043b20a3abd9
Assassin-1234 <chordsandfrets@gmail.com>
2020-07-28 01:34:02 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const { watch } = require("fs");
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("Raikyuu on youtube !", {type: "WATCHING"});
+    bot.user.setActivity("36guilds", {type: "WATCHING"});
 }
