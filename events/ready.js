ceb32370e6781dda0e8aca8bffe8a36ff306e2fc
Assassin-1234 <chordsandfrets@gmail.com>
2020-07-28 16:04:40 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,8 @@ const Discord = require("discord.js");
 const { watch } = require("fs");
 
 module.exports = bot => { 
-    console.log(`${bot.user.username} is online`)
-    //bot.user.setActivity("", {type: "WATCHING"});
+    bot.user.setActivity( `${client.guilds.cache.size} servers !`, {type: "WATCHING"});
 
-    let statuses = [
-        `${bot.guild.size}servers !`,
-        "+help",
-        `${bot.users.size} members !`
-    ]
-
-    setInterval(function() {
-        let status = statuses[Math.floor(Math.randon() * statuses.length)];
-        bot.user.setActivity(status, {type: "WATCHING"});
-    }, 5000)
+     console.log(`${bot.user.username} is online`)
+   
 }
