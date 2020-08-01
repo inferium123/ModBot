5d12b48ac32e315a1be0347274e6c14dfc583aa2
Assassin-1234 <chordsandfrets@gmail.com>
2020-08-02 00:02:08 +0530

    botinfo

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send(`${bot.guilds.cache.size} servers ${bot.users.cache.size} members !`)
+    return message.channel.send(````${bot.guilds.cache.size} servers ${bot.users.cache.size} members !````)
 }
 
 module.exports.config = {
-    name: "hi",
-    description: "says hi",
-    usage: "=hi",
+    name: "botinfo",
+    description: "botinfo",
+    usage: "=botinfo",
     accessableby: "Members",
-    aliases: ['h']
+    aliases: ['bi']
 }
