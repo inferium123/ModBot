81eb170ad1ffe9f7f4e339d080a78c7d67a4283e
Assassin-1234 <chordsandfrets@gmail.com>
2020-08-01 23:28:41 +0530

    Update hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -3,6 +3,7 @@ const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
     return message.channel.send(`${bot.guilds.cache.size} servers ${bot.users.cache.size} members !`)
+}
 
 module.exports.config = {
     name: "hi",
