d6000ba8fcd5ec6c9deb7f256223f9c630e5c79f
Assassin-1234 <chordsandfrets@gmail.com>
2020-08-02 00:04:14 +0530

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send(````${bot.guilds.cache.size} servers ${bot.users.cache.size} members !````)
+    return message.channel.send(`${bot.guilds.cache.size} servers ${bot.users.cache.size} members !`)
 }
 
 module.exports.config = {
