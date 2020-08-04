cc2964ab43f8a8be8a07b4ac688b66f025c9447f
Assassin-1234 <chordsandfrets@gmail.com>
2020-08-05 03:39:46 +0530

    Update alexa.js

--- commands/alexa.js
+++ commands/alexa.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send("is the coolest person alive becuz she helped build me !")
+    return message.channel.send("<@416792561047437322> is the coolest person alive becuz he helped build me !")
 }
 
 module.exports.config = {
