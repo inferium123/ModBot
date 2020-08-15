79a5ef710c4c86be8061249a261fa1dab0584bdf
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-08-16 03:42:48 +0530

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,18 +1,20 @@
-
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+    let channel = message.mentions.channels.first()
+
+    if(!channel) return message.reply("**Please Mention the channel.**")
+
     const verify = new Discord.MessageEmbed()
         .setColor("GREEN")
         .setDescription([
             "React to this message to get full access to the server :tada:"
         ])
-
-    let msgEmbed = await message.channel.send(verify)
-    msgEmbed.react(':VoteYes:722455899192557629')
+    let msgEmbed = await channel.send(verify)
+    msgEmbed.react('✅')
 
     const filter = (reaction, user) => {
-        return [':VoteYes:722455899192557629'].includes(reaction.emoji.name) && user.id === message.author.id;
+        return ['✅'].includes(reaction.emoji.name) && user.id === message.author.id;
     };
 
     message.awaitReactions(filter, {
@@ -23,11 +25,12 @@ module.exports.run = async (bot, message, args) => {
         .then(collected => {
             const reaction = collected.first();
 
-            if (reaction.emoji.name === ':tada:') {
+            if (reaction.emoji.name === '✅') {
                 const hotdogembed = new Discord.MessageEmbed()
                     .setTitle("You Gay")
                     .setColor("RANDOM")
                 message.channel.send(hotdogembed)
+                console.log("YOU GAY")
             }
         })
         .catch(collected => {
