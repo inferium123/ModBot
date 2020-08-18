fcfba8789eab7ea7e508d5ea7179a9ee7d0af50a
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-08-18 13:44:45 +0530

    opus added

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ bot.on("message", (message) => {
  
     if(command === "reroll"){
         let messageID = args[0];
-        client.giveawaysManager.reroll(messageID).then(() => {
+        bot.giveawaysManager.reroll(messageID).then(() => {
             message.channel.send("Success! Giveaway rerolled!");
         }).catch((err) => {
             message.channel.send("No giveaway found for "+messageID+", please check and try again");
