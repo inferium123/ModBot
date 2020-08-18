4a840373a20638f6a925cb80287b6f1713f8ced4
Assassin-1234 <68866924+Assassin-1234@users.noreply.github.com>
2020-08-18 20:39:26 +0530

    fixes

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const { watch } = require("fs");
 module.exports = bot => { 
 
      console.log(`${bot.user.username} is online`)
-     bot.user.setActivity( `${bot.guilds.cache.size} servers ${bot.users.cache.size} members !`, {type: "WATCHING"});
+     bot.user.setActivity( `${bot.guilds.cache.size} servers ${bot.users.cache.size} members !`, {type: "STREAMING", url: "https://www.twitch.tv/Assassin_118"});
      
 }
